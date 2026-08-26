3fe496a442d0c2fde558238fa5d91773c53d21c3
NathanFrazao <nathan.frazao@icloud.com>
2026-08-26 12:26:47 -0300

    Atividade1

--- ex1.py
+++ ex1.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+import numpy as np
+
+u1= int(input("Digite u1: "))
+u2= int(input("Digite u2: "))
+v1= int(input("Digite v1: "))
+v2= int(input("Digite v2: "))
+
+U= np.array([u1,u2])
+V= np.array([v1,v2])
+
+retorno = V - U
+print("Retorno", retorno)
